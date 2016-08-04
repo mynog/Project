086fe768d3f459cd9defa4a91af0b39d86760886
Sergey Efremov <efremov_su@mail.ru>
2016-08-04 09:42:45 +0300

    Fix AccountInfoServiceImpl class

--- src/main/java/com/victorku/musiccloud/service/AccountInfoServiceImpl.java
+++ src/main/java/com/victorku/musiccloud/service/AccountInfoServiceImpl.java
@@ -3,7 +3,9 @@ package com.victorku.musiccloud.service;
 import com.victorku.musiccloud.model.AccountInfo;
 import com.victorku.musiccloud.repository.AccountInfoRepisitory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AccountInfoServiceImpl implements AccountInfoService{
 
     @Autowired
